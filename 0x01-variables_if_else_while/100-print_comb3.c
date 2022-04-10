0d9f003c4ebb86672a22fcb6021794658c65a951
brytchukz <brightchukwu2015@gmail.com>
2022-04-10 04:56:00 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,11 +12,11 @@
  * Return: Always 0.
  */
 int main(void)
-{
+       {
 	int digit1, digit2;
 
 	for (digit1 = 0; digit1 < 9; digit1++)
-	{
+	        { 
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
@@ -28,11 +28,11 @@ int main(void)
 			putchar(',');
 			putchar(' ');
 		}
-	}
+	        }
 
 	putchar('\n');
 
 	return (0);
-}
+        }
 	
 
