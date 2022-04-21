3b593048a66ca4e9979e40c397d633805e4575b4
brytchukz <brightchukwu2015@gmail.com>
2022-04-20 17:23:29 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _strlen - length of a string
- * @s: A pointer to an int that will be changed/updated
- * Return: void that means our answer is correct
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ * Return: void
  */
 
-int _strlen(char *s);
+int _strlen(char *s)
 {
 	int i = 0;
 
