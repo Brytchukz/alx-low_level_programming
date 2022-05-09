cb7c6d95f1091515469a295c24f4307afacacdc7
brytchukz <brightchukwu2015@gmail.com>
2022-05-09 09:09:09 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,13 +9,13 @@
 
 void *malloc_checked(unsigned int b);
 {
-	char *p;
+	char *ptr;
 
-	p = malloc(b);
-	if (p == NULL)
+	ptr = malloc(b);
+	if (ptr == NULL)
 	{
 		exit(98);
 		return (NULL);
 	}
-	return (p);
+	return (ptr);
 }
