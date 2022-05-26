784122a2d185c934b22b293bcc12ec3aa2e72ec1
Brytchukz <101369922+Brytchukz@users.noreply.github.com>
2022-05-26 20:54:17 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,17 +7,16 @@
  *
  * Return: The number of nodes in the list_t list.
  */
-
 size_t print_listint(const listint_t *h)
 {
 	unsigned int count;
 
 	if (h == NULL)
 		return (0);
-	for (i = 0; h != NULL; i++)
+	for (count = 0; h != NULL; count++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return (i);
+	return (count);
 }
