f77789695d7b15f10d57e8285898e82aabd272bd
Brytchukz <101369922+Brytchukz@users.noreply.github.com>
2022-05-30 19:10:22 +0100

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,10 +8,11 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int mask;
-  mask = 50 / 10;
-  b = 20 / 4;
-  if(mask || b);
-  return(-1);
-  (mask && b);
-  return(1);
-  }
+
+	if (index > sizeof(unsigned int) * 8)
+		return (-1);
+	mask = 1;
+	mask = mask << index;
+	*n = ((*n) | mask);
+	return (1);
+}
